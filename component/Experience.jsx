5bd8a7f3235f16dd8c8faa26a9a4a63901724cfc
zaid009 <92687804+zaid009@users.noreply.github.com>
2026-01-08 19:56:02 +0500

    Update Experience.jsx

--- component/Experience.jsx
+++ component/Experience.jsx
@@ -7,7 +7,7 @@ const Experience = () => {
           <h2 className="title">Work <span className="accent">Experience</span></h2>
         </div>
         <div className="container">
-          <div className='exp-box'> 
+          <div className="exp-box"> 
           <div className="card" style={{ margin: '28px auto', maxWidth: '760px' }}>
             <div className="grid" style={{ gap: '8px' }}>
               <div className="grid" style={{ gap: '6px' }}>
